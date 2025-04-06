22ec21c0c6fb735a149ca20bfb068554148a6bb0
devCarlosHenriqueHC <dev.carloshenrique.hc@gmail.com>
2025-04-06 12:00:59 -0300

    atualização de mysql para postegres usando docker

--- src/main/java/com/financialcontrol/config/CorsConfig.java
+++ src/main/java/com/financialcontrol/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://192.168.100.22:3000")
+                        .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
